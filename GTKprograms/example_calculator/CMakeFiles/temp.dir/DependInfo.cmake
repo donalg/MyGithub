27a7d18151e7b54396e6683fea2e011f575b19e5
DonalG <donal-glavin@hotmail.com>
2017-01-31 17:06:32 +0000

    General Update 2 Android applications

---
+++ GTKprograms/example_calculator/CMakeFiles/temp.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/donalglavin/Documents/Programming/MyGithub/GTKprograms/example_calculator/temp.c" "/Users/donalglavin/Documents/Programming/MyGithub/GTKprograms/example_calculator/CMakeFiles/temp.dir/temp.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_REENTRANT"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/opt/X11/include/cairo"
+  "/opt/X11/include/pixman-1"
+  "/opt/X11/include"
+  "/opt/X11/include/freetype2"
+  "/opt/X11/include/libpng16"
+  "/usr/local/Cellar/gtk+3/3.22.6/include/gtk-3.0"
+  "/usr/local/Cellar/glib/2.50.2/include/gio-unix-2.0"
+  "/usr/local/Cellar/cairo/1.14.8/include/cairo"
+  "/usr/local/Cellar/libepoxy/1.3.1/include"
+  "/usr/local/Cellar/pango/1.40.3/include/pango-1.0"
+  "/usr/local/Cellar/harfbuzz/1.4.1/include/harfbuzz"
+  "/usr/local/Cellar/atk/2.22.0/include/atk-1.0"
+  "/usr/local/Cellar/gdk-pixbuf/2.36.2/include/gdk-pixbuf-2.0"
+  "/usr/local/Cellar/glib/2.50.2/include/glib-2.0"
+  "/usr/local/Cellar/glib/2.50.2/lib/glib-2.0/include"
+  "/usr/local/opt/gettext/include"
+  "/usr/local/Cellar/pcre/8.39/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
